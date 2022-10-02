e01dcbbfe5746e24d3b9cf8f01a7f9b77967ab37
Maitri Vira <maitrivira2@gmail.com>
2022-10-02 15:15:48 +0700

    check

--- Sources/Restaurant/Data/Locale/GetRestaurantsLocaleDataSource.swift
+++ Sources/Restaurant/Data/Locale/GetRestaurantsLocaleDataSource.swift
@@ -23,6 +23,7 @@ public struct GetRestaurantsLocaleDataSource: LocaleDataSource {
     }
     
     public func getRestaurants() -> Observable<[RestaurantModuleEntity]> {
+        print("masuk resto locale data source")
         return Observable<[RestaurantModuleEntity]>.create { observer in
 
             if let realm = self._realm {
@@ -31,6 +32,7 @@ public struct GetRestaurantsLocaleDataSource: LocaleDataSource {
                         .sorted(byKeyPath: "id", ascending: true)
                 }()
                 observer.onNext(restaurants.toArray(ofType: RestaurantModuleEntity.self))
+                print("data resto", restaurants.toArray(ofType: RestaurantModuleEntity.self))
                 observer.onCompleted()
             } else {
                 observer.onError(DatabaseError.requestFailed)
